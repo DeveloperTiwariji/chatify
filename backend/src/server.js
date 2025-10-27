e7d53ed7b65b09f969ed64e521a29921e319c5bc
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2025-10-27 19:47:26 +0530

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import messageRoutes from "./routes/message.route.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cookieParser from "cookie-parser";
 
 const __dirname= path.resolve();
 
@@ -14,6 +15,7 @@ const app = express();
 const PORT =ENV.PORT|| 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
